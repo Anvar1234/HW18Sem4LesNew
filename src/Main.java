5c43afcffb2e8aedc54b1e927e7a696cfc3561eb
Anvar <kingartaved@yandex.ru>
2023-04-06 01:31:37 +0500

    Правки2

--- src/Main.java
+++ src/Main.java
@@ -6,11 +6,11 @@ import java.util.LinkedList;
 
 public class Main {
     public static void main(String[] args) {
-//        LinkedList<Integer> newLL1 = new LinkedList<>(GetNewList.getArray());
-//        System.out.println("Первоначальный список_1: ");
-//        System.out.println(newLL1);
-//        System.out.println("Реверсивный список_1: ");
-//        ReverseList.reversList(newLL1);
+        LinkedList<Integer> newLL1 = new LinkedList<>(GetNewList.getArray());
+        System.out.println("Первоначальный список_1: ");
+        System.out.println(newLL1);
+        System.out.println("Реверсивный список_1: ");
+        ReverseList.reversList(newLL1);
 
         LinkedList<Integer> newLL2 = new LinkedList<>(GetNewList.getArray());
         System.out.println("Первоначальный список_2: ");
